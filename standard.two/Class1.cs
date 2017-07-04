8ffb5be31e8c2d087ca1bce666ca00db1eb451b4
Nick Josevski <nick@josevski.com>
2017-07-05 09:14:05 +1000

    init ; restore worked with MyGet NetCore configuration

---
+++ standard.two/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace standard.two
+{
+    public class Class1
+    {
+        public void Foo()
+        {
+            var bar = new System.DirectoryServices.DirectoryEntry(@"c:\temp");
+        }
+    }
+}
